7efb48fc1f9b0e403ebd2db7409f446597b41d69
amaliatyler <amaliatyler@gmail.com>
2023-08-11 16:55:13 +0300

    spinner and error message

---
+++ src/components/errorMessage/ErrorMessage.js
@@ -0,0 +1,8 @@
+import error from './error.gif';
+const ErrorMessage = () => {
+        return (
+                <img className="error-img" src={error} alt="Error"/>
+        )
+}
+
+export default ErrorMessage;
