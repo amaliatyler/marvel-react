8f7ce0e039f1fd211acd43f0857632082c1310be
amaliatyler <amaliatyler@gmail.com>
2023-08-14 19:20:22 +0300

    homework

--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -79,11 +79,15 @@ class CharInfo extends Component {
 const View = ({ char }) => {
 
     const {name, description, thumbnail, homepage, wiki, comics} = char;
+    let imgStyle = {'objectFit': 'cover'};
+        if(thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
+            imgStyle = {'objectFit': 'contain'};
+        }
 
   return (
     <>
       <div className="char__basics">
-        <img src={thumbnail} alt={name} />
+        <img src={thumbnail} alt={name} style={imgStyle}/>
         <div>
           <div className="char__info-name">{name}</div>
           <div className="char__btns">
@@ -101,16 +105,16 @@ const View = ({ char }) => {
       </div>
       <div className="char__comics">Comics:</div>
       <ul className="char__comics-list">
-        { comics.map((item, i) => {
-            return (
-                <li key={i} className="char__comics-item">
-                    {item.name}
-                </li>
-            )
-        })}
-        <li className="char__comics-item">
-          All-Winners Squad: Band of Heroes (2011) #3
-        </li>
+        { comics.length > 0 ? (
+            comics.map((item, i) => {
+                if(i > 10) return;
+                return (
+                    <li key={i} className="char__comics-item">
+                        {item.name}
+                    </li>
+                )
+            })
+        ) : <div>There are no comics about this character</div>}
       </ul>
     </>
   );
